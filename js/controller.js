7ed3f79dc096156763a6ecf4007b25a88366c3ca
el coruco <el.coruco@gmail.com>
2014-07-31 19:04:47 -0500

    ya ajusta los colores según la tabla establecida; ahora tengo que reescribir una función más sencilla para que también pegue los nombres de los estados sin programar nada extra. Bueno, yo me entiendo

--- js/controller.js
+++ js/controller.js
@@ -40,11 +40,6 @@ define(function(require){
 
       // render the map
       this.render_map();
-
-      // set the map base color; maybe this one must be don directly into the SVG
-      var states = d3.select('#Mexico')
-          .selectAll('path')
-            .attr('style', 'fill: #fff; cursor: pointer');
     },
 
     //
@@ -55,12 +50,44 @@ define(function(require){
       var select      = this.$('#year-select-map'),
           index       = select.val(),
           year        = this.$('option[value="' + index + '"]').html(),
+          collection  = this.collection,
           total_model = this.collection.findWhere({estado_id : 0, categoria_id : 3}),
           total       = total_model.get('data_1990_2013')[index];
 
       // render the TOTAL TOURIST LABEL
       this.$('#tourist-year-map-label').html(year);
       this.$('#tourist-total-map-label').html(total);
+
+      // set the colors on the map
+
+      var states = d3.select('#Mexico')
+        .selectAll('path')
+        .attr('style', function(){
+          var id     = this.getAttribute('id'),
+              models = collection.where({estado_id : Number(id)}),
+              data   = _.map(models, function(d,i){
+                return d.get('data_1990_2013')[index];
+              }),
+              total = d3.sum(data),
+              fill  = '';
+
+              if(total >= 12000000){
+                fill = "#27769f";
+              }
+              else if(total >= 4000000){
+                fill = "#4ea6d9";
+              }
+              else if(total >= 500000){
+                fill = "#b1d8ee";
+              }
+              else if(total >= 2000){
+                fill = "#d8ebf6";
+              }
+              else{
+                fill = "#fff";
+              }
+          return 'fill: ' + fill + '; cursor: pointer';
+        });
     }
   });
 
